1fe9b7fdb10f5e8027bc2f0f062679405ec1490a
cforee <cforee@gmail.com>
2014-10-06 05:19:12 -0700

    Update floor and wall textures

--- config.rb
+++ config.rb
@@ -58,6 +58,9 @@ configure :build do
   # For example, change the Compass output style for deployment
   # activate :minify_css
 
+  # Compass
+  activate :compass
+
   # Minify Javascript on build
   # activate :minify_javascript
 
